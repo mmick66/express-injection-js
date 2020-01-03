354d08a0bc91c47b54873edc15025b971a201f7d
Michael Michailidis <mmick66@gmail.com>
2020-01-03 10:55:03 +0200

    Fetching data with jQuery

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ import { API_TOKEN, SEARCH_QUERY, SEARCH_SERVICE } from "./injection-tokens";
 import SearchService from "./search.service";
 import HomeController from "./home.controller";
 
+// Get Env variables
 const port = process.env.PORT || 3000;
 const dirViews = __dirname + '/../views';
+const api_token = process.env.GOOGLE_API_TOKEN || 'AIzaSyAouh0wETa-n24yqZ8tAxfJWC39JkOQhO8';
 
 const app = express();
 
@@ -15,11 +17,11 @@ app.get('/', (req, res) => {
     res.sendFile('home.html',{ root: dirViews });
 });
 
-app.get('/search', (req, res) => {
+app.get('/search', (request, response) => {
 
     const providers = [
-        { provide: API_TOKEN, useValue: 'AIzaSyAouh0wETa-n24yqZ8tAxfJWC39JkOQhO8' },
-        { provide: SEARCH_QUERY, useValue: req.query },
+        { provide: API_TOKEN, useValue: api_token },
+        { provide: SEARCH_QUERY, useValue: request.query },
         { provide: SEARCH_SERVICE, useClass: SearchService },
         HomeController
     ] as Provider[];
@@ -31,9 +33,12 @@ app.get('/search', (req, res) => {
         .search()
         .then(res => {
             console.log(res);
+            response.send(res);
         }).catch(err => {
             console.error(err);
+            response.send(err);
         });
+
 
 
 });
